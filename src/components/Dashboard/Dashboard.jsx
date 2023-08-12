dae35c1194ccb55eb612f6bd057b03794955b273
arpit gupta <mycontact.arpit@gmail.com>
2023-08-12 20:26:48 +0530

    feat: dashboard updated

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,18 +1,42 @@
+import React from "react";
+import { ProductContext } from "../../Context/ProductContext";
+
 import "./Dashboard.css";
 
 export const Dashboard = () => {
+    const { products } = React.useContext(ProductContext);
+    console.log(products);
+    const totalStock = products.reduce((total, product) => {
+        // + to covert string to number
+        return total + Number(product.stock);
+    }, 0);
+    const totalDeliver = products.reduce(
+        // + to covert string to number
+        (total, product) => total + Number(product.delivered),
+        0
+    );
+    const lowStockItems = products.reduce(
+        (total, product) => (product.stock <= 10 ? total + 1 : total),
+        0
+    );
     return (
         <div className="small-card-container">
             <div className="detail-card">
-                <p className="count stock">283</p>
+                <div className="number-card" style={{ color: "green" }}>
+                    {totalStock}
+                </div>
                 <p className="title">Total Stock</p>
             </div>
             <div className="detail-card">
-                <p className="count delivered">9329</p>
+                <div className="number-card" style={{ color: "#FFA81E" }}>
+                    {totalDeliver}
+                </div>
                 <p className="title">Total Delivered</p>
             </div>
             <div className="detail-card">
-                <p className="count low-stock">83</p>
+                <div className="number-card" style={{ color: "red" }}>
+                    {lowStockItems}
+                </div>
                 <p className="title">Low Stock Items</p>
             </div>
         </div>
